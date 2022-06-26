9d7a7f8850e09420fffafc7cd59a7307a520df66
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-06-26 10:52:43 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+	let positiveN;
+	let result = '';
+	n < 0 ? positiveN = String(n * (-1)) : positiveN = String(n);
+	for (let i = 0; i < positiveN.length; i++) {
+	  result = positiveN[i] + result;
+	}
+	return Number(result);
 }
